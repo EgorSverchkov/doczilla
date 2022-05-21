425416ac397aef572cb6ec7f69d60046c2821dfc
Egor Sverchkov <sverch322@gmail.com>
2022-05-21 17:05:23 +0300

    Create project with spring inializr

---
+++ src/main/java/com/example/doczilla/DoczillaApplication.java
@@ -0,0 +1,13 @@
+package com.example.doczilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoczillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoczillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doczilla/DoczillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doczilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoczillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
